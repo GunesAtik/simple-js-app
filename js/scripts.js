6cf4d23b2a544749a5c0dd4ab453bf391893cf25
GunesAtik <guenes_atik@hotmail.de>
2020-11-02 20:32:42 +0100

    create pokemonList and add objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-alert('Hello world!');
+let pokemonList = [
+  {name:'Bulbasaur',
+  height: 0.7,
+  weight:6.9,
+  types: ['grass', 'poison']},
 
-let favoriteFood = 'Burger';
-document.write(favoriteFood);
+  {name:'Pikachu',
+  height: 0.4,
+  weight: 6,
+  types: ['electric']}
+
+  {name: 'jigglypuff',
+  height: 0.5,
+  weight: 5.5,
+  types: ['fairy', 'normal']}
+
+  {name: 'horsea',
+  height: 0.4,
+  weight: 8,
+  types: ['water']}
+];
