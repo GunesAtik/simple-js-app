629fe1f1e85131db6d85e2d62d0745d367659a15
GunesAtik <guenes_atik@hotmail.de>
2020-11-09 15:10:53 +0100

    add pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,20 @@ let pokemonList = [
   height: 0.4,
   weight: 8,
   types: ['water']}
+
+  {name: 'Charizard',
+  height: 1.7,
+  weight: 90.5,
+  types: ['fire', 'flying']}
+
+  {name: 'Butterfree',
+  height: 1.1,
+  weight: 32,
+  types: 'bug', 'flying'}
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(i);
+  document.write(pokemonList[i].name + '(height: ')' + pokemonList[i].height + ')');
 }
+
+if (pokemonList[i]height < )
