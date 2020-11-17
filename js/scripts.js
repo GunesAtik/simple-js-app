ee6dd6c787ea600665b753f64f3f205c64da784b
GunesAtik <guenes_atik@hotmail.de>
2020-11-17 19:01:17 +0100

    adjust pokemon properties and add to loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonList = [
   {name:'Bulbasaur',
   height: 0.7,
   weight:6.9,
-  types: ['grass', 'poison']},
+  types: ['grass', ' poison']},
 
   {name:'Pikachu',
   height: 0.4,
@@ -13,7 +13,7 @@ let pokemonList = [
   {name: 'Jigglypuff',
   height: 0.5,
   weight: 5.5,
-  types: ['fairy', 'normal']},
+  types: ['fairy', ' normal']},
 
   {name: 'Horsea',
   height: 0.4,
@@ -23,12 +23,12 @@ let pokemonList = [
   {name: 'Charizard',
   height: 1.7,
   weight: 90.5,
-  types: ['fire', 'flying']},
+  types: ['fire', ' flying']},
 
   {name: 'Butterfree',
   height: 1.1,
   weight: 32,
-  types: ['bug', 'flying']}
+  types: ['bug', ' flying']}
 ];
 
 function add(pokemon) {
@@ -45,11 +45,16 @@ function add(pokemon) {
  };
 })();
 
-pokemonRepository.add({name: 'Oddish', height: 0.5, weight: 5.4, types: ['grass', 'poison']});
+pokemonRepository.add({name: 'Oddish', height: 0.5, weight: 5.4, types: ['grass'
+, 'poison']});
 
 pokemonRepository.getAll().forEach(function(pokemon) {
 if (pokemon.height > 1.0){
-document.write("<p>" + pokemon.name + " ( weight: " + pokemon.weight + ", height: " + pokemon.height + ") - Wow, thats big! </p>");
+
+document.write('<p>' + pokemon.name + '<br>' + 'weight: ' + pokemon.weight + '<br>' + 'height: '
++ pokemon.height + ' - Wow, thats big!' + '<br>' + 'Types: ' + pokemon.types + '</p>');
+
 } else {
-document.write("<p>" + pokemon.name + " ( weight: " + pokemon.weight + ", height: " + pokemon.height + ") </p>")};
+document.write('<p>' + pokemon.name + '<br>' + 'weight: ' + pokemon.weight + '<br>' + 'height: '
+ + pokemon.height + '<br>' + 'Types: ' + pokemon.types + '</p>')};
 });
