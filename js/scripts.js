a3c745838f6a0cb3f16027f7d48a280f9964223e
GunesAtik <guenes_atik@hotmail.de>
2020-11-18 16:04:49 +0100

    append button and list items

--- js/scripts.js
+++ js/scripts.js
@@ -54,4 +54,6 @@ pokemonRepository.getAll().forEach(function(pokemon) {
   let button = document.createElement('button');
   button.innerText = 'placeholder';
   button.classList.add('button-class);')
+  listItem.appendChild(button);
+  pokemonList.appendChild(listItem);
 });
