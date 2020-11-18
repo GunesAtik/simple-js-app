b080c78de86725f10d82a03d52301751bfaea0a3
GunesAtik <guenes_atik@hotmail.de>
2020-11-18 16:09:33 +0100

    replace placeholder with pokemon name

--- js/scripts.js
+++ js/scripts.js
@@ -52,8 +52,9 @@ pokemonRepository.getAll().forEach(function(pokemon) {
   let pokemonList = document.querySelector('.pokemon-list');
   let listItem = document.createElement('li');
   let button = document.createElement('button');
-  button.innerText = 'placeholder';
+  button.innerText = pokemon.name;
   button.classList.add('button-class);')
   listItem.appendChild(button);
   pokemonList.appendChild(listItem);
+
 });
