7a12003cf70142e5057765040abb9adfb5023c1b
GunesAtik <guenes_atik@hotmail.de>
2020-11-23 17:12:11 +0100

    add loadDetails()

--- js/scripts.js
+++ js/scripts.js
@@ -73,10 +73,24 @@ function loadList() {
   })
 }
 
+function loadDetails(item) {
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response {
+    return response.json();
+  }).then(function (details) {
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+
  return {
    add: add,
    getAll: getAll,
-   loadList: loadList
+   loadList: loadList,
+   loadDetails: loadDetails
   };
 })();
 
