7ae022acc2baecfa2ec1c4308d28e92f67a7a8e8
GunesAtik <guenes_atik@hotmail.de>
2020-12-14 20:15:08 +0100

    corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 var pokemonRepository = (function() {
 let repository = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-let modalContainer = document.querySelector('#modal-container');
 function add(pokemon) {
-  if (
+  if(
     typeof pokemon === "object" &&
     "name" in pokemon
   ){
@@ -34,7 +33,7 @@ function addListItem(pokemon) {
   button.addEventListener('click', function(event) {
   showDetails(pokemon);
   })
-  }`
+}
 
 function loadList() {
   return fetch(apiUrl).then(function (response) {
@@ -46,7 +45,6 @@ function loadList() {
         detailsUrl: item.url
       };
       add(pokemon);
-      console.log(pokemon);
     });
   }).catch(function (e) {
     console.error(e);
@@ -60,7 +58,9 @@ function loadDetails(item) {
   }).then(function (details) {
     item.imageUrl = details.sprites.front_default;
     item.height = details.height;
-    item.types = details.types;
+    item.types = details.types.map(function(pokemon) {
+    return pokemon.type.name;
+    });
   }).catch(function (e) {
     console.error(e);
   });
@@ -68,81 +68,27 @@ function loadDetails(item) {
 
 function showDetails(pokemon) {
   pokemonRepository.loadDetails(pokemon).then(function () {
-    let modalContainer = document.querySelector('#modal-container');
     showModal(pokemon);
   })}
 
-    // function showModal(item) {
-      //modalContainer.innerHTML = '';
-      //let modal = document.createElement('div');
-      //modal.classList.add('modal');
+function showModal(item) {
+let modalBody = $('.modal-body');
+let modalTitle = $('.modal-title');
+modalTitle.empty();
+modalBody.empty();
 
-      //let closeButtonElement = document.createElement('button');
-      //closeButtonElement.classList.add('modal-close');
-      //closeButtonElement.innerText = 'Close';
-      //closeButtonElement.addEventListener('click', hideModal);
+let nameElement = $('<h1>' + item.name + '</h1>');
+let imageElement = $('<img class="modal-image" style="width:50%">');
+imageElement.attr('src', item.imageUrl);
+let heightElement = $('<p>' + 'Height: ' + item.height + '</p>');
+let typesElement = $(
+'<p>' + 'Types: ' + item.types + '</p>');
 
-      //let titlePokemon = document.createElement('h1');
-      //titlePokemon.innerText = item.name;
-
-      //let heightPokemon = document.createElement('p');
-      //heightPokemon.innerText = 'Height: ' + item.height;
-
-      //let imagePokemon = document.createElement('img');
-      //imagePokemon.setAttribute('src', item.imageUrl);
-
-      //modal.appendChild(closeButtonElement);
-      //modal.appendChild(titlePokemon);
-      //modal.appendChild(heightPokemon);
-      //modal.appendChild(imagePokemon);
-      //modalContainer.appendChild(modal);
-
-      //modalContainer.classList.add('is-visible');
-    //}
-
-    //function hideModal() {
-      //modalContainer.classList.remove('is-visible');
-    //}
-
-  //  window.addEventListener('keydown', (e) => {
-  //  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      //hideModal();
-    //}
-  //});
-
-  //modalContainer.addEventListener('click',(e) => {
-  //  let target = e.target;
-  //  if (target === modalContainer) {
-    //  hideModal();
-  //  }
-  //});
-
-  function showModal(item) {
-    let modalBody = $('.modal-body');
-    let modalTitle = $('.modal-title');
-    let modalHeader = $('.modal-header');
-    modalTitle.empty();
-    modalBody.empty();
-
-    let nameElement = $('<h1>' + item.name + '</h1>');
-    let imageElementFront = $('<img class="modal-image" style="width:50%">');
-    imageElementFront.attr('src', item.imageUrlFront);
-    let imageElementBack = $('<img class="modal-image" style="width:50%">');
-    imageElementBack.attr('src', item.imageUrlBack);
-    let heightElement = $('<p>' + 'Height: ' + item.height + '</p>');
-    let weightElement = $('<p>' + 'Weight: ' + item.weight + '</p>');
-    let typesElement = $(
-      '<p>' + 'Types: ' + item.types.toString().replace(',', ', ') + '</p>');
-    let abilitiesElement = $('<p>' + 'abilities :' + item.abilities + '</p>');
-
-    modalTitle.append(nameElement);
-    modalBody.append(imageElementFront);
-    modalBody.append(imageElementBack);
-    modalBody.append(heightElement);
-    modalBody.append(weightElement);
-    modalBody.append(typesElement);
-    modalBody.append(abilitiesElement);
-  }
+modalTitle.append(nameElement);
+modalBody.append(imageElement);
+modalBody.append(heightElement);
+modalBody.append(typesElement);
+}
 
  return {
    add: add,
